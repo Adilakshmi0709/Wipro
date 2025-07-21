100bcb7276cf320bc37d04ab5267d953cafbfa3d
Adilakshmi0709 <adilakshminagavenkata137@gmail.com>
2025-07-21 18:35:10 +0530

    first commit

---
+++ Day1/HelloWorld.cs
@@ -0,0 +1,11 @@
+using System;
+namespace HelloWorld
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey Class");
+        }
+    }
+}
