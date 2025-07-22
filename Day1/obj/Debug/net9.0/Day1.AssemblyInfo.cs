896deaf3af203c5edb5b86648d8220138e2a4cba
Adilakshmi0709 <adilakshminagavenkata137@gmail.com>
2025-07-22 15:53:44 +0530

    first commit

--- Day1/obj/Debug/net9.0/Day1.AssemblyInfo.cs
+++ Day1/obj/Debug/net9.0/Day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+100bcb7276cf320bc37d04ab5267d953cafbfa3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
