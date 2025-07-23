8bca907c333278babba46172f3f5f6f34aa36bda
Adilakshmi0709 <adilakshminagavenkata137@gmail.com>
2025-07-23 14:14:48 +0530

    first commit

---
+++ Day5/PolymorphismExample.cs
@@ -0,0 +1,35 @@
+﻿// using System;
+
+// namespace oops.poly
+// {
+//     class InfoPrinter
+//     {
+//         public int Pages;
+//         InfoPrinter()
+//         {
+//             Console.WriteLine("Constructor Called !");
+//         }
+//         InfoPrinter(int Pagess)
+//         {
+//             this.Pages = Pagess;
+//             this.Pages = this.Pages + 1;
+//             Console.WriteLine("Total Pages Printed : " + this.Pages);
+//         }
+//         public void Print(string name, int empId)
+//         {
+//             Console.WriteLine("Name : " + name + ", EmpId : " + empId);
+//         }
+//         public void Print(int empId, string name)
+//         {
+//             Console.WriteLine("Name : " + name + ", EmpId : " + empId);
+//         }
+//         static void Main()
+//         {
+//             InfoPrinter obj = new InfoPrinter();
+//             obj.Print("Ammu", 1001);
+//             obj.Print(1002, "Cherry");
+//             InfoPrinter obj1 = new InfoPrinter(23);
+//             InfoPrinter obj2 = new InfoPrinter(12);
+//         }
+//     }
+// }
---
+++ Day5/VirtualExample.cs
@@ -0,0 +1,31 @@
+// public class Person
+// {
+//     public virtual void GetRole()
+//     {
+//         Console.WriteLine("Person class");
+//     }
+// }
+// public class Teacher : Person
+// {
+//     public override void GetRole()
+//     {
+//         Console.WriteLine("She is a teacher");
+//     }
+// }
+// public class Mother : Person
+// {
+//     public override void GetRole()
+//     {
+//         Console.WriteLine("She is a Mother");
+//     }
+// }
+// public class MainProgram
+// {
+//     static void Main()
+//     {
+//         Person p = new Teacher();
+//         Person p1 = new Mother();
+//         p.GetRole();
+//         p1.GetRole();
+//     }
+// }
